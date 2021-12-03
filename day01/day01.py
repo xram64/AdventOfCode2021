69cee09539366bd5af5a550c3179d8d48dc66ff8
xram64 <33797815+xram64@users.noreply.github.com>
2021-12-03 13:22:41 -0500

    Refactoring with map()

--- day01/day01.py
+++ day01/day01.py
@@ -1,7 +1,7 @@
 ## Advent of Code 2021: Day 1
 ## https://adventofcode.com/2021/day/1
 ## Jesse Williams | github.com/xram64
-## Answers: [Part 1]: 1448, [Part 2]:
+## Answers: [Part 1]: 1448, [Part 2]: 1471
 
 def count_increases(depths):
     increases = 0
@@ -16,22 +16,20 @@ def count_increases(depths):
 
 if __name__ == "__main__":
     depths = []
+
+    def parse(line):
+        if line: return int(line)
+
     with open('day01_input.txt', 'r') as f:
-        for line in f.readlines():
-            if line: depths.append( int(line) )
+        depths = list( map(parse, f.readlines()) )
 
     ## Part 1
     increases = count_increases(depths)
-
     print(f"[Part 1] There are {increases} increased measurements.")
-
 
     ## Part 2
     window_sums = []
-
     for i in range(2, len(depths)):
         window_sums += [ depths[i-2] + depths[i-1] + depths[i] ]
-
     increases = count_increases(window_sums)
-
     print(f"[Part 2] There are {increases} increased measurements within three-measurement windows.")
