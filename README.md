dbd5ecaf5adcbec9cfd8a5bdc7f7234079bf0392
xram64 <33797815+xram64@users.noreply.github.com>
2021-12-03 16:35:41 -0500

    Day 3

--- README.md
+++ README.md
@@ -9,3 +9,19 @@
 [Part 1] There are 1448 increased measurements.
 [Part 2] There are 1471 increased measurements within three-measurement windows.
 ```
+
+### Day 2
+```
+> python day02.py
+
+[Part 1] Final horizontal position: 1850. Final depth: 927. Product: 1714950.
+[Part 2] Final horizontal position: 1850. Final depth: 692961. Product: 1281977850.
+```
+
+### Day 3
+```
+> python day03.py
+
+[Part 1] Epsilon rate: ε = 1300. Gamma rate: γ = 2795. Power consumption: 3633500.
+[Part 2] Oxygen generator rating: 1327. CO2 scrubber rating: 3429. Life support rating: 4550283.
+```
--- day03/day03.py
+++ day03/day03.py
@@ -1,12 +1,12 @@
 ## Advent of Code 2021: Day 3
 ## https://adventofcode.com/2021/day/3
 ## Jesse Williams | github.com/xram64
-## Answers: [Part 1]: 3633500, [Part 2]:
+## Answers: [Part 1]: 3633500, [Part 2]: 4550283
 
 import sys
 
+# Return most commonly-found bit, breaking ties in favor of '1'
 def get_most_common_bit(numbers, pos):
-    # Find the most common bit (0 or 1) in position 'pos' of each number in 'numbers'
     frequency_of = [0, 0]  # frequencies for each bit [<0's>, <1's>]
 
     for num in numbers:
@@ -16,14 +16,19 @@ def get_most_common_bit(numbers, pos):
     most_common_bit = 0 if (frequency_of[0] > frequency_of[1]) else 1
     return most_common_bit
 
+# Return least commonly-found bit, breaking ties in favor of '0'
+def get_least_common_bit(numbers, pos):
+    return (get_most_common_bit(numbers, pos) ^ 1)
+
+
 if __name__ == '__main__':
     with open('day03_input.txt', 'r') as f:
         def parse(line): return [int(bit) for bit in list(line) if (bit != '\n')]
         diag_numbers = list( map(parse, f.readlines()) )
     diag_numbers_len = len(diag_numbers[0])
 
-
-    ## Part 1
+    ##############
+    ### Part 1 ###
     most_common_bits = ''
     for pos in range(diag_numbers_len):
         most_common_bits += str( get_most_common_bit(diag_numbers, pos) )
@@ -32,12 +37,50 @@ if __name__ == '__main__':
     ε = int(most_common_bits, 2)
 
     # Gamma rate: Least common bits from input
-    γ = ε ^ int('1'*diag_numbers_len, 2)  # XOR with 111111111111
+    γ = ε ^ int('1'*diag_numbers_len, 2)  # invert bits in ε
 
     print(f"[Part 1] Epsilon rate: ε = {ε}. Gamma rate: γ = {γ}. Power consumption: {ε*γ}.")
 
+    ##############
+    ### Part 2 ###
+    oxy_rating_filtered_nums = diag_numbers
+    co2_rating_filtered_nums = diag_numbers
 
-    ## Part 2
-    # for i in range(diag_numbers_len):
-    #     # Filter numbers
-    #     filter( (lambda num: num[i] > ), diag_numbers )
+    ## Oxygen generator rating
+    for pos in range(diag_numbers_len):
+        # Find most common bit (MCB) and filter numbers not matching bit criteria
+        mcb = get_most_common_bit(oxy_rating_filtered_nums, pos)
+        oxy_rating_filtered_nums = list( filter(lambda num: num[pos] == mcb, oxy_rating_filtered_nums) )
+
+        if len(oxy_rating_filtered_nums) == 1:
+            # Convert final element matching bit criteria to a decimal integer
+            oxy_rating = int(''.join( [str(n) for n in oxy_rating_filtered_nums[0]] ), 2)
+            break
+
+        elif len(oxy_rating_filtered_nums) <= 0:
+            print('Error: Oxygen rating list empty.')
+            sys.exit()
+    else:
+        print('Error: Multiple numbers found matching bit criteria for oxygen rating.')
+        sys.exit()
+
+    ## CO2 scrubber rating
+    for pos in range(diag_numbers_len):
+        # Find least common bit (LCB) and filter numbers not matching bit criteria
+        lcb = get_least_common_bit(co2_rating_filtered_nums, pos)
+        co2_rating_filtered_nums = list( filter(lambda num: num[pos] == lcb, co2_rating_filtered_nums) )
+
+        if len(co2_rating_filtered_nums) == 1:
+            # Convert final element matching bit criteria to a decimal integer
+            co2_rating = int(''.join( [str(n) for n in co2_rating_filtered_nums[0]] ), 2)
+            break
+
+        elif len(co2_rating_filtered_nums) <= 0:
+            print('Error: CO2 rating list empty.')
+            sys.exit()
+    else:
+        print('Error: Multiple numbers found matching bit criteria for CO2 rating.')
+        sys.exit()
+
+
+    print(f"[Part 2] Oxygen generator rating: {oxy_rating}. CO2 scrubber rating: {co2_rating}. Life support rating: {oxy_rating*co2_rating}.")
