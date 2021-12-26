37d5ed4ed4e939ae3a9c274f45947fc2fd9cfa44
xram64 <33797815+xram64@users.noreply.github.com>
2021-12-26 16:10:15 -0500

    Day 7

--- day07/day07.py
+++ day07/day07.py
@@ -1,10 +1,15 @@
 ## Advent of Code 2021: Day 7
 ## https://adventofcode.com/2021/day/7
 ## Jesse Williams | github.com/xram64
-## Answers: [Part 1]: 329389, [Part 2]:
+## Answers: [Part 1]: 329389, [Part 2]: 86397080
 
 from sys import maxsize as MAXINT
 from functools import reduce
+
+def triangleSum(n):
+    # https://en.wikipedia.org/wiki/Triangular_number
+    # Σ_n{k} = n(n+1)/2
+    return int(n*(n+1)/2)
 
 if __name__ == '__main__':
     with open('day07_input.txt', 'r') as f:
@@ -32,3 +37,16 @@ if __name__ == '__main__':
             minimum = (final_pos, total_fuel)
 
     print(f"[Part 1] Final horizontal position: {minimum[0]}. Total fuel consumed: {minimum[1]}.")
+
+
+    ## Part 2
+    minimum = (0, MAXINT)  # (target position, total fuel consumed)
+
+    for final_pos in range(pos_min, pos_max+1):
+        pos_changes = map(lambda pos: triangleSum(abs(pos - final_pos)), positions)
+        total_fuel = sum(pos_changes)
+
+        if (total_fuel < minimum[1]):
+            minimum = (final_pos, total_fuel)
+
+    print(f"[Part 2] Final horizontal position: {minimum[0]}. Total fuel consumed: {minimum[1]}.")
